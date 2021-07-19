3bbb91602e9800dab05884fc5478d8fb33c84eb5
Travis <travis.moulton002@gmail.com>
2021-07-19 17:14:06 -0400

    input test passing

--- src/components/UI/Input/Input.test.js
+++ src/components/UI/Input/Input.test.js
@@ -1,29 +1,47 @@
 import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
 
 import Input from './Input';
-import { customRender, fireEvent } from '../../../shared/testUtils';
+import { customRender } from '../../../shared/testUtils';
+
+function MockComponent() {
+  const [mockInput, setMockInput] = useState({
+    elementType: 'input',
+    elementConfig: {
+      type: 'email',
+      placeholder: 'mockPlaceholder',
+    },
+    value: '',
+    testid: 'mockInput',
+  });
+
+  const changed = (e) => setMockInput({ ...mockInput, value: e.target.value });
+
+  return (
+    <Input
+      elementType={mockInput.elementType}
+      elementConfig={mockInput.elementConfig}
+      changed={(e) => changed(e)}
+      value={mockInput.value}
+      testid={mockInput.testid}
+    />
+  );
+}
 
 describe('<Input />', () => {
-  const propsProto = {
-    value: 'mockVal',
-    label: 'mockLabel',
-    testid: 'mockTest',
-    changed: (val) => (this.value = val),
-  };
+  test('renders a text input and updates the value on user input', async () => {
+    const { getByTestId } = customRender(<MockComponent />);
 
-  test('renders a text input and updates the value', () => {
-    const props = {
-      ...propsProto,
-      elementType: 'input',
-      elementConfig: {
-        type: 'text',
-        placeholder: 'mock placeholder',
-      },
-    };
+    const input = getByTestId('mockInput');
 
-    const { getByTestId, getByText } = customRender(<Input {...props} />);
+    expect(input).toBeInTheDocument();
+    expect(input.value).not.toBe('someOtherVal');
+    expect(input.value).toBe('');
 
-    expect(getByTestId('mockTest')).toBeInTheDocument();
-    expect(getByText('mockVal')).toBeInTheDocument();
+    userEvent.type(input, 'someOtherMockVal');
+
+    expect(input).toHaveValue('someOtherMockVal');
+
+    expect(getByTestId('mockInput').value).toBe('someOtherMockVal');
   });
 });
