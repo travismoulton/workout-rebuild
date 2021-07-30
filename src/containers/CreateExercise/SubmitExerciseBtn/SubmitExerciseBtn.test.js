a500c4291c663ca45024d701203935d0d72c91d4
Travis <travis.moulton002@gmail.com>
2021-07-30 09:07:51 -0400

    added testing for SubmitExerciseBtn

--- src/containers/CreateExercise/SubmitExerciseBtn/SubmitExerciseBtn.test.js
+++ src/containers/CreateExercise/SubmitExerciseBtn/SubmitExerciseBtn.test.js
@@ -1,14 +1,47 @@
-import { customRender } from '../../../shared/testUtils';
+import { customRender, fireEvent } from '../../../shared/testUtils';
 import SubmitExerciseBtn from './SubmitExerciseBtn';
-
-//1: Test tooltip renders as expected
-//2: Check nameTaken works as expected
-//3: Test that if the name is taken there is no call to submit exercise
-//4: Test that if there is an axios error there is no call to submit exercise
 
 describe('<SubmitExerciseBtn />', () => {
   test('renders', () => {
     const { getByText } = customRender(<SubmitExerciseBtn />);
     expect(getByText('Submit Exercise')).toBeInTheDocument();
+  });
+
+  test('tooltip renders as expected', async () => {
+    const { getByText, queryByText } = customRender(<SubmitExerciseBtn />);
+    const btn = getByText('Submit Exercise');
+    expect(btn).toHaveAttribute('disabled');
+
+    fireEvent.mouseOver(btn);
+
+    const message = queryByText(
+      'Name and category are required to create an exercise'
+    );
+
+    expect(message).toBeInTheDocument();
+
+    fireEvent.mouseOut(btn);
+
+    expect(message).not.toBeInTheDocument();
+  });
+
+  test('tooltip renders as expected when passed props', async () => {
+    const { getByText, queryByText } = customRender(
+      <SubmitExerciseBtn nameIsValid />
+    );
+    const btn = getByText('Submit Exercise');
+
+    fireEvent.mouseOver(btn);
+
+    const message = queryByText('Category is required to create an exercise');
+
+    expect(message).toBeInTheDocument();
+  });
+
+  test('is not disabled if the form is valid', () => {
+    const { getByText } = customRender(<SubmitExerciseBtn formIsValid />);
+    const btn = getByText('Submit Exercise');
+
+    expect(btn).not.toHaveAttribute('disabled');
   });
 });
